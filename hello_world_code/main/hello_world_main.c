26507f98ff35ab6c71e98b63fae9c4c108455e50
Nin Tran <nintran52@Nins-MacBook-Pro.local>
2022-07-18 10:07:57 +0700

    feat(button): update event

--- hello_world_code/main/hello_world_main.c
+++ hello_world_code/main/hello_world_main.c
@@ -11,134 +11,134 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/timers.h"
-// #include "freertos/event_groups.h"
+#include "freertos/event_groups.h"
 #include "esp_system.h"
 #include "esp_spi_flash.h"
 #include "output_iot.h"
-// #include "input_iot.h"
+#include "input_iot.h"
 
-TimerHandle_t xTimers[2];
-// EventGroupHandle_t xEventGroup;
+// TimerHandle_t xTimers[2];
+EventGroupHandle_t xEventGroup;
 
-// #define BIT_EVENT_BUTTON_PRESS (1 << 0)
-// #define BIT_EVENT_BUTTON_DOUBLE_PRESS (1 << 1)
+#define BIT_EVENT_BUTTON_PRESS (1 << 0)
+#define BIT_EVENT_BUTTON_DOUBLE_PRESS (1 << 1)
 
-void vTimerCallback(TimerHandle_t xTimer)
-{
-   /* Optionally do something if the pxTimer parameter is NULL. */
-   configASSERT(xTimer);
-   int ulCount = (uint32_t)pvTimerGetTimerID(xTimer);
-   if (ulCount == 0)
-   {
-      output_io_toggle(13);
-   }
-   else if (ulCount == 1)
-   {
-      printf("Hello\n");
-   }
-}
-
-void vTask1(void *pvParameters)
-{
-   for (;;)
-   {
-      printf("Task 1\n");
-      vTaskDelay(1000 / portTICK_PERIOD_MS);
-   }
-}
-
-void vTask2(void *pvParameters)
-{
-   for (;;)
-   {
-      printf("Task 2\n");
-      vTaskDelay(1000 / portTICK_PERIOD_MS);
-   }
-}
-
-void vTask3(void *pvParameters)
-{
-   for (;;)
-   {
-      printf("Task 3\n");
-      vTaskDelay(1000 / portTICK_PERIOD_MS);
-   }
-}
-
-// void vTaskButton(void *pvParameters)
+// void vTimerCallback(TimerHandle_t xTimer)
 // {
-//     for (;;)
-//     {
-//         EventBits_t uxBits = xEventGroupWaitBits(
-//             xEventGroup,
-//             BIT_EVENT_BUTTON_PRESS | BIT_EVENT_BUTTON_DOUBLE_PRESS,
-//             pdTRUE,
-//             pdFALSE,
-//             portMAX_DELAY);
-
-//         if (uxBits & BIT_EVENT_BUTTON_PRESS)
-//         {
-//             printf("BUTTON PRESS\n");
-//             output_io_toggle(15);
-//         }
-//         if (uxBits & BIT_EVENT_BUTTON_DOUBLE_PRESS)
-//         {
-//             printf("BUTTON DOUBLE PRESS\n");
-//             output_io_toggle(13);
-//         }
-//     }
+//    /* Optionally do something if the pxTimer parameter is NULL. */
+//    configASSERT(xTimer);
+//    int ulCount = (uint32_t)pvTimerGetTimerID(xTimer);
+//    if (ulCount == 0)
+//    {
+//       output_io_toggle(13);
+//    }
+//    else if (ulCount == 1)
+//    {
+//       printf("Hello\n");
+//    }
 // }
 
-// void button_callback(int pin)
+// void vTask1(void *pvParameters)
 // {
-//     if (pin == GPIO_NUM_0)
-//     {
-//         BaseType_t pxHigherPriorityTaskWoken;
-//         xEventGroupSetBitsFromISR(xEventGroup, BIT_EVENT_BUTTON_PRESS, &pxHigherPriorityTaskWoken);
-//     }
+//    for (;;)
+//    {
+//       printf("Task 1\n");
+//       vTaskDelay(1000 / portTICK_PERIOD_MS);
+//    }
 // }
+
+// void vTask2(void *pvParameters)
+// {
+//    for (;;)
+//    {
+//       printf("Task 2\n");
+//       vTaskDelay(1000 / portTICK_PERIOD_MS);
+//    }
+// }
+
+// void vTask3(void *pvParameters)
+// {
+//    for (;;)
+//    {
+//       printf("Task 3\n");
+//       vTaskDelay(1000 / portTICK_PERIOD_MS);
+//    }
+// }
+
+void vTaskButton(void *pvParameters)
+{
+   for (;;)
+   {
+      EventBits_t uxBits = xEventGroupWaitBits(
+          xEventGroup,
+          BIT_EVENT_BUTTON_PRESS | BIT_EVENT_BUTTON_DOUBLE_PRESS,
+          pdTRUE,
+          pdFALSE,
+          portMAX_DELAY);
+
+      if (uxBits & BIT_EVENT_BUTTON_PRESS)
+      {
+         printf("BUTTON PRESS\n");
+         output_io_toggle(15);
+      }
+      if (uxBits & BIT_EVENT_BUTTON_DOUBLE_PRESS)
+      {
+         printf("BUTTON DOUBLE PRESS\n");
+         output_io_toggle(13);
+      }
+   }
+}
+
+void button_callback(int pin)
+{
+   if (pin == GPIO_NUM_21)
+   {
+      BaseType_t pxHigherPriorityTaskWoken;
+      xEventGroupSetBitsFromISR(xEventGroup, BIT_EVENT_BUTTON_PRESS, &pxHigherPriorityTaskWoken);
+   }
+}
 
 void app_main(void)
 {
-   xTimers[0] = xTimerCreate("TimerBlink", pdMS_TO_TICKS(500), pdTRUE, (void *)0, vTimerCallback);
-   xTimers[1] = xTimerCreate("TimerPrint", pdMS_TO_TICKS(1000), pdTRUE, (void *)1, vTimerCallback);
+   // xTimers[0] = xTimerCreate("TimerBlink", pdMS_TO_TICKS(500), pdTRUE, (void *)0, vTimerCallback);
+   // xTimers[1] = xTimerCreate("TimerPrint", pdMS_TO_TICKS(1000), pdTRUE, (void *)1, vTimerCallback);
 
    output_io_create(13);
-   // input_io_create(GPIO_NUM_0, ANY_EDGE);
-   // input_set_callback(button_callback);
+   input_io_create(GPIO_NUM_21, ANY_EDGE);
+   input_set_callback(button_callback);
 
-   xTimerStart(xTimers[0], 0);
-   xTimerStart(xTimers[1], 0);
+   // xTimerStart(xTimers[0], 0);
+   // xTimerStart(xTimers[1], 0);
 
-   // xEventGroup = xEventGroupCreate();
+   xEventGroup = xEventGroupCreate();
 
+   // xTaskCreate(
+   //     vTask1,   /* Function that implements the task. */
+   //     "vTask1", /* Text name for the task. */
+   //     1024,     /* Stack size in words, not bytes. */
+   //     NULL,     /* Parameter passed into the task. */
+   //     4,        /* Priority at which the task is created. */
+   //     NULL);    /* Used to pass out the created task's handle. */
+
+   // xTaskCreate(
+   //     vTask2,   /* Function that implements the task. */
+   //     "vTask2", /* Text name for the task. */
+   //     1024,     /* Stack size in words, not bytes. */
+   //     NULL,     /* Parameter passed into the task. */
+   //     5,        /* Priority at which the task is created. */
+   //     NULL);    /* Used to pass out the created task's handle. */
+   // xTaskCreate(
+   //     vTask3,   /* Function that implements the task. */
+   //     "vTask3", /* Text name for the task. */
+   //     1024,     /* Stack size in words, not bytes. */
+   //     NULL,     /* Parameter passed into the task. */
+   //     6,        /* Priority at which the task is created. */
+   //     NULL);    /* Used to pass out the created task's handle. */
    xTaskCreate(
-       vTask1,   /* Function that implements the task. */
-       "vTask1", /* Text name for the task. */
-       1024,     /* Stack size in words, not bytes. */
-       NULL,     /* Parameter passed into the task. */
-       4,        /* Priority at which the task is created. */
-       NULL);    /* Used to pass out the created task's handle. */
-
-   xTaskCreate(
-       vTask2,   /* Function that implements the task. */
-       "vTask2", /* Text name for the task. */
-       1024,     /* Stack size in words, not bytes. */
-       NULL,     /* Parameter passed into the task. */
-       5,        /* Priority at which the task is created. */
-       NULL);    /* Used to pass out the created task's handle. */
-   xTaskCreate(
-       vTask3,   /* Function that implements the task. */
-       "vTask3", /* Text name for the task. */
-       1024,     /* Stack size in words, not bytes. */
-       NULL,     /* Parameter passed into the task. */
-       6,        /* Priority at which the task is created. */
-       NULL);    /* Used to pass out the created task's handle. */
-                 // xTaskCreate(
-                 //     vTaskButton,   /* Function that implements the task. */
-                 //     "vTaskButton", /* Text name for the task. */
-                 //     1024 * 2,      /* Stack size in words, not bytes. */
-                 //     NULL,          /* Parameter passed into the task. */
-                 //     7,             /* Priority at which the task is created. */
-                 //     NULL);         /* Used to pass out the created task's handle. */
+       vTaskButton,   /* Function that implements the task. */
+       "vTaskButton", /* Text name for the task. */
+       1024,          /* Stack size in words, not bytes. */
+       NULL,          /* Parameter passed into the task. */
+       4,             /* Priority at which the task is created. */
+       NULL);         /* Used to pass out the created task's handle. */
 }
